e2d346bbce8b58527d992145b3d9f3ec484bad9f
Akshay Solanke <akshaysolan804@gmail.com>
2025-07-02 22:09:44 +0530

    add api

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -1,11 +1,14 @@
 import { Request, RequestHandler } from "express";
 import User from "../model/userSchema";
 import { sendResponse } from "../utils/sendResponse";
+import crypto from 'crypto';
+import { hashPassword } from "../utils/passwordHelper";
 
 interface RegisterReq extends Request {
     body: {
         email: string;
         password: string;
+       
     };
 }
 
@@ -13,11 +16,18 @@ export const signUpUser: RequestHandler = async (req: RegisterReq, res) => {
     try {
         const { email, password } = req.body;
         const existingUser = await User.findOne({ email });
+        
         if (existingUser) {
             return sendResponse(res, 400, false, "User already exists");
         }
 
-        await User.create({ email, password });
+        const hashedPassword = await hashPassword(password);
+
+        await User.create({ 
+            email, 
+            password : hashedPassword,
+            token:crypto.randomBytes(16).toString("hex")
+        });
 
         return sendResponse(res, 200, true, 'User created successfully');
 
---
+++ backend/src/utils/passwordHelper.ts
@@ -0,0 +1,10 @@
+const bcrypt = require('bcrypt');
+
+
+export const hashPassword = async(
+    orignalPassword : string
+):Promise<string> =>{
+    const hashedPassword = await bcrypt.hash(orignalPassword, 16);
+
+    return hashedPassword;
+};
